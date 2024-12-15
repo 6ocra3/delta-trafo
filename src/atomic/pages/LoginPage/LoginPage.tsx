ea1a346afc8961dd49625e1b1cf9b20962b5d82d
6ocra3 <6o3kmm@mail.ru>
2024-12-15 15:03:25 +0300

    fix icons

--- src/atomic/pages/LoginPage/LoginPage.tsx
+++ src/atomic/pages/LoginPage/LoginPage.tsx
@@ -4,6 +4,8 @@ import { useAppDispatch } from '../../../store';
 import { ILoginRequest } from '../../../api/auth/types';
 import { LocalStorageTokenKey, loginUser } from '../../../store/slices/auth';
 import { useFormik } from 'formik';
+import hidden from "/src/assets/images/icons/hidden2.svg"
+import logo from "/src/assets/images/icons/logo-colored.svg"
 import { useNavigate } from 'react-router-dom'; 
 
 const LoginPage: React.FC = () => {
@@ -60,7 +62,9 @@ const LoginPage: React.FC = () => {
                         value={formik.values.password}
                         onChange={formik.handleChange}
                         className="login__input password__input input" id="password" type={isHidden ? "password" :  "text"}/>
-                   <button onClick={() => setIsHidden(!isHidden)} className="password__hidden" data-target="#password"></button>
+                   <button onClick={() => setIsHidden(!isHidden)} className="password__hidden" data-target="#password">
+                      <img src={hidden}/>
+                   </button>
                 </label>
               </div>
               <button type='submit' className="login__btn" >Войти</button>
@@ -68,7 +72,7 @@ const LoginPage: React.FC = () => {
             </form>
             <div className="login__footer">
               <a className="login__footer-link logo" href="#">
-                <img className="login__footer-img logo__img" src="src/assets/images/icons/logo-colored.svg" alt=""/>
+                <img className="login__footer-img logo__img" src={logo} alt=""/>
               </a>
             </div>
           </div>
